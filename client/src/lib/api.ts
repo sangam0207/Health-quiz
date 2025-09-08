f7aed67770ab37eceb38d25a5ad4c9c05d1e163e
sangam0207 <ssrv2024@gmail.com>
2025-09-08 22:40:20 +0530

    feat: creat quiz application

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL ;
 
 export async function fetchQuestions() {
   const res = await fetch(`${API_URL}/quiz/questions`);
